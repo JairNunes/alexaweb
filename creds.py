0b5425cfcce1d0edcd6b88d6ddee2b78a5e02c46
Jair Nunes <jairnunes1@yahoo.com.br>
2019-01-12 20:03:14 -0200

    updated creds

---
+++ creds.py
@@ -0,0 +1,9 @@
+#Alexa
+Security_Profile_Description ="Alexa Voice Service Sample App Security Profile "
+Security_Profile_ID  = "amzn1.application.50fe8531f0674517849cec737d60b7b2"
+Client_ID = "amzn1.application-oa2-client.a875c053e3514a18a526d6a928fa53d8"
+Client_Secret = "d8ea148405cba183e145a5923b753b94a1e330110ea955aafa631ca4f6b60b3d"
+Product_ID = "EchoRasp"
+
+#Redis
+redis_url = "redis://h:p2bcfe2e012921cfb9762ff762ca11d1a523f29d8a8272b0e0ae777877db3703b@ec2-174-129-114-5.compute-1.amazonaws.com:44529"
